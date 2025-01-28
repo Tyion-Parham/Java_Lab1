00be90ad9a57ee20abdddd43fbf84835fb3615f2
Tyion-Parham <112714623+Tyion-Parham@users.noreply.github.com>
2025-01-27 22:34:43 -0600

    Apart of the deletion series

--- Denomination.java
+++
@@ -1,9 +0,0 @@
-public record Denomination(String noteName, double noteValue) {
-
-    public String toString() {
-        return noteName;
-    }
-    public Double getNoteValue() {
-        return noteValue;
-    }
-}
